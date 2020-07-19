444116476b5a036474a7f680278ea17f0faa6ffb
asrafkaizen <asrafkaizen@gmail.com>
2020-07-19 07:16:33 -0800

    app basically done. just wants ui improvements

--- src/app/pages/idea-list/idea-list.page.ts
+++ src/app/pages/idea-list/idea-list.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { IdeaService, Idea } from 'src/app/services/idea.service';
 import { Observable } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
  
 @Component({
   selector: 'app-idea-list',
@@ -11,10 +13,19 @@ export class IdeaListPage implements OnInit {
  
   public ideas: Observable<Idea[]>;
  
-  constructor(private ideaService: IdeaService) { }
+  constructor(private ideaService: IdeaService,
+    public afAuth: AngularFireAuth,
+    public router: Router
+    ) { }
  
   ngOnInit() {
     this.ideas = this.ideaService.getIdeas();
-  }
+  };
+
+  logout() {
+    return this.afAuth.auth.signOut().then(() => {
+      this.router.navigate(['login']);
+    })
+  };
 }
 
